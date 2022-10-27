bbb7744c5dd196a324bf5a9f0780227c65c71d7f
youldaw <68648932+youldaw@users.noreply.github.com>
2022-10-27 11:26:04 +0500

    pc ready
    ready

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@ $(function (){
         $('.top-left').toggleClass('active');
     });
 
+    $('#switch-mode').on('click',function(e){
+        e.preventDefault();
+        $('.wrap').toggleClass('light');
+    });
+
+
+    $(function() {
+        var rangePercent = $('[type="range"]').val();
+        $('[type="range"]').on('change input', function() {
+            rangePercent = $('[type="range"]').val();
+            $('.range-percent').text(rangePercent);
+            // $('[type="range"]').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
+        });
+    });
 });
 
