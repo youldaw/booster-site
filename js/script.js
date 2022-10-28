aad3544364127e95fe0b908411152053152d5f0d
youldaw <68648932+youldaw@users.noreply.github.com>
2022-10-28 18:56:45 +0500

    sadasdsad
    asdasdasdas

--- js/script.js
+++ js/script.js
@@ -1,14 +1,43 @@
 $(function (){
     
-    $('.menu-mob, .menu-closer').on('click',function(e){
+    $('.sub-menu-link').on('click',function(e){
         e.preventDefault();
-        $('.top-left').toggleClass('active');
+        $(this).parent().siblings().removeClass('active');
+        $(this).parent().addClass('active');
     });
 
     $('#switch-mode').on('click',function(e){
         e.preventDefault();
         $('.wrap').toggleClass('light');
     });
+
+    $('.welcome-btns .metamask-btn').on('click',function(e){
+        e.preventDefault();
+        $('.mob-block').addClass('hide');
+        $('.side-top').addClass('show');
+        $('.performens-history').addClass('show');
+        $('.hello-block').addClass('show');
+    });
+
+    $('#mob-pools').on('click',function(e){
+        e.preventDefault();
+        $('.mob-block, .side-top, .performens-history, .hello-block').addClass('hide');
+        $('.booster-sec').addClass('active-pools');
+    });
+
+    $('#mob-more').on('click',function(e){
+        e.preventDefault();
+        $(this).parent().parent().parent().toggleClass('active');
+    });
+
+    $(".metamask-btn").prop("disabled", true);
+	$(".welcome-check input[type='checkbox']").on('change', function () {
+        if ($(".agree:checked").length > 0) {
+          $(".metamask-btn").prop("disabled", false);
+        } else {
+          $(".metamask-btn").prop("disabled", true);
+        }
+	});
 
 
     $(function() {
