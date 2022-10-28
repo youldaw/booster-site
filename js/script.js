799df23dd1c5a83eb9c31fe4b5772cfee4ee4fba
youldaw <68648932+youldaw@users.noreply.github.com>
2022-10-28 21:56:31 +0500

    dsadasd
    asdasdasdad

--- js/script.js
+++ js/script.js
@@ -11,18 +11,47 @@ $(function (){
         $('.wrap').toggleClass('light');
     });
 
-    $('.welcome-btns .metamask-btn').on('click',function(e){
+    $('.welcome-btns .metamask-btn, #mob-overview').on('click',function(e){
         e.preventDefault();
         $('.mob-block').addClass('hide');
-        $('.side-top').addClass('show');
-        $('.performens-history').addClass('show');
-        $('.hello-block').addClass('show');
+        $('.booster-sec').addClass('active-overview');
     });
 
     $('#mob-pools').on('click',function(e){
         e.preventDefault();
-        $('.mob-block, .side-top, .performens-history, .hello-block').addClass('hide');
+        $('.mob-block').addClass('hide');
         $('.booster-sec').addClass('active-pools');
+    });
+
+    $('#mob-history').on('click',function(e){
+        e.preventDefault();
+        $('.mob-block').addClass('hide');
+        $('.booster-sec').addClass('active-history');
+    });
+
+    $('.side-tab li a').on('click',function(e){
+        e.preventDefault();
+        $('.booster-side-col').addClass('hide');
+        $('.booster-main').addClass('active');
+    });
+
+    $('.back-position').on('click',function(e){
+        e.preventDefault();
+        $('.booster-side-col').removeClass('hide');
+        $('.booster-main').removeClass('active');
+    });
+    
+    $(document).mouseup(function (e){
+        var div = $(".booster-sec, .booster-main, .booster-side-col, #mob-more");
+        if (!div.is(e.target) 
+            && div.has(e.target).length === 0) {
+            div.removeClass('active-pools');
+            div.removeClass('active-overview');
+            div.removeClass('active-history');
+            div.removeClass('active-exchange');
+            div.removeClass('active');
+            div.removeClass('hide');
+        }
     });
 
     $('#mob-more').on('click',function(e){
